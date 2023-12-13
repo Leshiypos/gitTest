8e832bee6556cf95d366cbe09dc3dc3f2520459b
Wp_devsite <leshiy_pos@mail.ru>
2023-12-13 20:32:44 +0300

    add description for some files

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   /* Вторичное меню */
   const menu = document.querySelector(".menu__list");
   var menuact = $(".menu__list .active"); //устанавливаем подчеркивания на активную ссылку
-  var x = menuact.width() + "px";
-  var y = menuact[0].offsetLeft + "px"; // чтобы обратится к  свойству offsetLeft тк это свойство JS а не JQ то к элементу DOM  нужно обратится как к массиву - те через 0 элемент
-  menu.style.setProperty("--underline-width", x);
-  menu.style.setProperty("--underline-offset-x", y);
+  var x = menuact.width() + "px"; //определяем ширину текущей ссылки
+  var y = menuact[0].offsetLeft + "px"; // определяем расстояние от края текущй ссылки до края блока (чтобы обратится к  свойству offsetLeft тк это свойство JS а не JQ то к элементу DOM  нужно обратится как к массиву - те через 0 элемент)
+  menu.style.setProperty("--underline-width", x); // устанавливаем значение для переменной CSS
+  menu.style.setProperty("--underline-offset-x", y);// устанавливаем значение для переменной CSS
 
-    menu.addEventListener("mouseover", (event) => {
+    menu.addEventListener("mouseover", (event) => {             // обработчик события наведения на ссылку
       if (event.target.classList.contains("menu__link")) {
         menu.style.setProperty(
           "--underline-width",
@@ -20,7 +20,7 @@ $(document).ready(function(){
       }
     });
 
-    menu.addEventListener("mouseleave", () =>{
+    menu.addEventListener("mouseleave", () =>{                // обработчик события при сходе с сылки
       menu.style.setProperty("--underline-width", x);
       menu.style.setProperty("--underline-offset-x", y)
     }
